0b6d347f454f1b963e9cfbcf6544d69f732d0573
Calvin Roberts <roberts.calvin85@gmail.com>
2021-12-13 23:31:56 +0000

    Add animation delay and duration to function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function rain() {
-  let amount = 20;
+  let amount = 200;
   let body = document.querySelector('body');
   let i = 0;
   while (i < amount) {
@@ -7,9 +7,13 @@ function rain() {
 
     let size = Math.random() * 5;
     let posX = Math.floor(Math.random() * window.innerWidth);
+    let delay = Math.random() * -20;
+    let duration = Math.random() * 5;
 
     drop.style.width = 0.2 + size + 'px';
     drop.style.left = posX + 'px';
+    drop.style.animationDelay = delay + 's';
+    drop.style.animationDuration = 1 + duration + 's';
     body.appendChild(drop);
     i++;
   }
